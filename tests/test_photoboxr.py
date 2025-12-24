58f1023c2154118d590bbb754ca5bab2f94cd478
Your Name <rubygems@chrislee.dhs.org>
2025-12-24 16:57:56 -0500

    removed testing path

--- tests/test_photoboxr.py
+++ tests/test_photoboxr.py
@@ -5,4 +5,4 @@ sys.path.append('src')
 from photoboxy.photoboxy import generate_album
 shutil.rmtree("tests/output")
 shutil.rmtree(".db")
-generate_album(source_dir="***REMOVED***", dest_dir="tests/output")
+generate_album(source_dir="tests/input", dest_dir="tests/output")
